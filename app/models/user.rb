d3a8178668b845f5b40aeb57615d79b1b8dab7f5
Yaroslav Andreychuk <niliukhin@gmail.com>
2025-09-29 20:35:06 +0300

    fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :passive_subscriptions, class_name: "Subscription", foreign_key: :followed_id, dependent: :destroy
   has_many :followers, through: :passive_subscriptions, source: :follower
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :email, presence: { message: "не может быть пустым" }
   validates :password, presence: { message: "не может быть пустым" }, on: :create
 end
